267979a899ec74d1bb60c147fafe7275dfb08474
FedericoTapia <89428215+FedericoTapia@users.noreply.github.com>
2021-10-25 19:24:45 -0300

    Add files via upload

---
+++ cars.php
@@ -0,0 +1,33 @@
+<?php
+$cars = array(
+    "1" => array(
+        'modelo' => 'Nissan 370z',
+        'año' => '2012',
+        'kilometros' => '29.000',
+        'imagen' => 'images/index/370z.jpg',
+        'price' => 'USD$3500',
+        'carroceria'=>'Deportivo',
+        'localizacion'=>'La Plata'
+    ),
+    "2" => array(
+        'modelo' => 'Volkswagen Gol G3',
+        'año' => '2000',
+        'kilometros' => '355.000',
+        'imagen' => "images/ventas/golG3.jpg",
+        'price' => '$298.000',
+        'carroceria'=>'Hatchback',
+        'localizacion'=>'Chaves'
+        
+    ),
+    "3" => array(
+        'modelo' => 'Volkswagen Gol G8',
+        'año' => '2018',
+        'kilometros' => '15.000',
+        'imagen' => "images/ventas/golG8.jpeg",
+        'price' => '$1.250.000',
+        'carroceria'=>'Utilitario',
+        'localizacion'=>'Tres Arroyos'
+    ),
+    
+    
+);
